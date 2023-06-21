576c4de4d83aaa2f8b6c80a2957e3156ce3e98f6
suonbo <87938340+suonbo@users.noreply.github.com>
2023-06-21 14:13:42 +0700

    Update __init__.py

--- date_extractor/__init__.py
+++ date_extractor/__init__.py
@@ -367,12 +367,14 @@ def extract_dates(
     matches = []
     completes = []
     partials = []
+    date_factors = []
 
     # print "about to finditer"
     for match in regex.finditer(regex.compile(patterns["date"], flags), text):
         if debug:
             print("\n\nmatch is " + str(match.groupdict()))
         # this goes through the dictionary and removes empties and changes the keys back, e.g. from month_myd to month
+        date_factors.append(match.groupdict())
         match = dict((k, num(v)) for k, v in match.groupdict().items() if num(v))
 
         if all(k in match for k in ("day", "month", "year")):
@@ -431,7 +433,7 @@ def extract_dates(
     if debug:
         print("extract_dates returning: " + str(results))
 
-    return results
+    return results, date_factors
 
 
 e = extract_dates
